a710c615b4b3acc7663e8130ead8a742374cde42
Gaidaenko <45212496+Gaidaenko@users.noreply.github.com>
2021-09-28 09:16:45 +0300

    Перебор вариантов паролей

--- Guess the password xlsx/Form1.Designer.cs
+++ Guess the password xlsx/Form1.Designer.cs
@@ -30,49 +30,98 @@ namespace Guess_the_password_xlsx
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(46, 41);
+            this.button1.Location = new System.Drawing.Point(49, 57);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Select file xlsx";
+            this.button1.Text = "Выберете файл xlsx";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(179, 57);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 43);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Выберете файл txt с паролями";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(125, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Подобрать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(72, 166);
+            this.label1.Location = new System.Drawing.Point(43, 157);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(37, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Result";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество попыток:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // button2
+            // label2
             // 
-            this.button2.Location = new System.Drawing.Point(242, 41);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(103, 43);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Select file txt";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ваш пароль:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(122, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(345, 452);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,8 +130,12 @@ namespace Guess_the_password_xlsx
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Guess the password xlsx/Form1.cs
+++ Guess the password xlsx/Form1.cs
@@ -1,46 +1,145 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using Microsoft.Office.Interop.Excel;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace Guess_the_password_xlsx
 {
     public partial class Form1 : Form
     {
-        public static string tryPassword;
-
-        public static string file;
+        public static string tryPassword;                                                                          
+        public static string [] textArr;                                                                            
+        public static string fileXlsx;
+        public static string fileTxt;
+        public static int i = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void openTxt()
+        {
+            OpenFileDialog openFileTxt = new OpenFileDialog();
+            openFileTxt.Filter = "Формат txt(*.txt)|*.txt|Все файлы(*.*)|*.*";                 
+            openFileTxt.Title = "Выберете файл";
+
+            if (openFileTxt.ShowDialog() == DialogResult.OK)
+            {
+                fileTxt = openFileTxt.FileName;
+            }
+        }
+        private void openXlsx()
+        {
+            OpenFileDialog openFileXlsx = new OpenFileDialog();
+            openFileXlsx.Filter = "Формат xlsx(*.xlsx)|*.xlsx|Все файлы(*.*)|*.*";                
+            openFileXlsx.Title = "Выберете файл";
+
+            if (openFileXlsx.ShowDialog() == DialogResult.OK)
+            {
+                fileXlsx = openFileXlsx.FileName;
+            }
+        }
+        private void Try()
+        {
+            try
+            {
+                using (StreamReader text = new StreamReader(fileTxt))
+                {
+                    var result = text.ReadToEnd();
+                    result.ToArray();
+
+                    if (String.IsNullOrEmpty(result))
+                    {
+                        MessageBox.Show("Вероятно, файл с вариантами паролей пуст.");
+                        return;
+                    }
+
+                    textArr = result.Split();
+
+                Next: for (i = i; i < textArr.Length; i++)
+                    {
+                        tryPassword = textArr[i];
+
+                        try
+                        {
+                            Excel.Application xlsApp = new Excel.Application();
+                            Workbook ObjWorkBook = xlsApp.Workbooks.Open
+                                (Filename: fileXlsx,
+                                 UpdateLinks: 0,
+                                 ReadOnly: true,
+                                 Format: 5,
+                                 Password: tryPassword,
+                                 WriteResPassword: true,
+                                 false,
+                                 Origin: XlPlatform.xlWindows,
+                                 Delimiter: "",
+                                 Editable: true,
+                                 Notify: false,
+                                 Converter: 0,
+                                 AddToMru: true,
+                                 Local: false,
+                                 CorruptLoad: false);
+
+                            Process.Start(fileXlsx);
+
+                            label4.Text = tryPassword;
+                            return;
+                        }
+                        catch
+                        {
+                            i++;
+                            label2.Text = i.ToString();
+
+                           if (textArr[i] == null)                                                              
+                           {
+                                //go to exception                                                                     
+                           }
+
+                            goto Next; 
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Отсутствуют подходящие пароли.");
+            }
+        }
+            
         private void button1_Click(object sender, EventArgs e)
         {
-            OpenFileDialog openFile = new OpenFileDialog();
-            openFile.Filter = "Формат xlsx(*.xlsx)|*.xlsx|Все файлы(*.*)|*.*";
-            openFile.Title = "Выберете файл";
-
-            if (openFile.ShowDialog() == DialogResult.OK)
-            {
-               file = openFile.FileName;
-            }
-
-            Excel.Application xlsApp = new Excel.Application();
-            Workbook ObjWorkBook = xlsApp.Workbooks.Open(file, 0, false, 5, tryPassword, "", false, XlPlatform.xlWindows, "", true, false, 0, true, false, false);
-            Worksheet ObjWorkSheet;
+            openXlsx();          
         }
-
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openTxt();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Try();
+        }
         private void label1_Click(object sender, EventArgs e)
         {
-           
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
